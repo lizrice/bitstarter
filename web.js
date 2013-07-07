f16d74a7da0eb5bfa3d3a23f8c3929f2430d7d53
Liz Rice <liz@lizrice.com>
2013-07-07 10:19:22 +0000

    Read Hello World from index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var hello_world = fs.readFileSync("index.html");
+  var buffer = new Buffer(hello_world);
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
