6e9ae996d99f897500cd063f26e83db068ec2d16
Liz Rice <liz@lizrice.com>
2013-07-07 12:35:03 +0000

    HW3 - part 3 - grade app running on Heroku

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -44,16 +45,37 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkString = function(htmlstring, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+    for (var ii in checks) {
+      var present = htmlstring(checks[ii]).length > 0;
+      out[checks[ii]] = present;
     }
     return out;
 };
+
+var checkHtmlFile = function(htmlfile, checksfile) {
+    htmlstring = cheerioHtmlFile(htmlfile);
+    console.log(htmlstring);
+    return checkString(htmlstring, checksfile);
+};
+
+var checkUrl = function(url, checksfile) {
+      result = restler.get(url).on('complete', function(result) { 
+      if (result instanceof Error) {
+        sys.puts('Error: ' + result.message);
+        return
+      } 
+
+      fs.writeFileSync("out.html", result);
+      checkJson = checkHtmlFile("out.html", checksfile);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson)
+
+    });
+};
+
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -65,10 +87,17 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <URL>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+      console.log("Checking the URL");
+      console.log("Checks: " + program.checks);
+      checkUrl(program.url, program.checks);
+    } else {
+      var checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
